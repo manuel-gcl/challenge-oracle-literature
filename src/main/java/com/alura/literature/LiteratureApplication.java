c0b31cde7ff651f0893330dd888de31863eeffb6
manuel-gcl <manuelgonzalez2849@gmail.com>
2025-01-28 13:52:52 -0300

    Update program closure

--- src/main/java/com/alura/literature/LiteratureApplication.java
+++ src/main/java/com/alura/literature/LiteratureApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LiteratureApplication implements CommandLineRunner {
@@ -16,13 +17,14 @@ public class LiteratureApplication implements CommandLineRunner {
     private AuthorRepository authorRepository;
 
     public static void main(String[] args) {
-
-        SpringApplication.run(LiteratureApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(LiteratureApplication.class, args);
+        SpringApplication.exit(context, () -> 0);
     }
 
     @Override
     public void run(String... args) throws Exception {
         Main main = new Main(bookRepository, authorRepository);
         main.run();
+        System.exit(0);
     }
 }
